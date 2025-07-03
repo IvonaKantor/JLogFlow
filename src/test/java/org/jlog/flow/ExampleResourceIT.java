a60e835be11d2fce085edea0346b661332e2fabc
ivona <ik87056@student.uwb.edu.pl>
2025-07-03 19:46:20 +0200

    init commit

---
+++ src/test/java/org/jlog/flow/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package org.jlog.flow;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
