adf0dfbc1ec240ac6c442fc5ef1c05487e7f435a
ivona <ik87056@student.uwb.edu.pl>
2025-07-08 23:43:37 +0200

    created a topic and added it to the events list

--- consumer/src/main/java/org/jlog/flow/ExampleResource.java
+++ consumer/src/main/java/org/jlog/flow/ExampleResource.java
@@ -3,11 +3,19 @@ package org.jlog.flow;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @ApplicationScoped
 public class ExampleResource {
 
-    @Incoming("hello")
+    public static final String TOPIC_HELLO = "hello";
+
+    public static final List<String> events = new ArrayList<>();
+
+    @Incoming(TOPIC_HELLO)
     public void processLog(String message) {
+        events.add(message);
         System.out.println("Log received: " + message);
     }
 }
