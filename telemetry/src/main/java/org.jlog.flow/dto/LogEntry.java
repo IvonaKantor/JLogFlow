26d064785916a9c22319d5a3a4930472419a866e
ivona <ik87056@student.uwb.edu.pl>
2025-10-01 16:29:28 +0200

    added dto class for representing a log entry

---
+++ telemetry/src/main/java/org.jlog.flow/dto/LogEntry.java
@@ -0,0 +1,12 @@
+package org.jlog.flow.dto;
+
+import java.time.LocalTime;
+
+public class LogEntry {
+    private String appName;
+    private String level;
+    private String message;
+    private LocalTime timestamp;
+    private String threadName;
+
+}
