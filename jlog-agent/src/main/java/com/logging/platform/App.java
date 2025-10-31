6de77c3cacdb869f5a7e596822b9632f89032fe1
ivona <ik87056@student.uwb.edu.pl>
2025-11-01 00:09:40 +0100

    update

--- jlog-agent/src/main/java/com/logging/platform/App.java
+++ jlog-agent/src/main/java/com/logging/platform/App.java
@@ -1,9 +1,7 @@
 package com.logging.platform;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
