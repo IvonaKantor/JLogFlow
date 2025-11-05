1fe64449fac9f5ebe1a38e80ce7fcabd29d611d4
ivona <ik87056@student.uwb.edu.pl>
2025-11-05 01:29:54 +0100

    example application logging

--- jlog-agent/src/main/java/com/logging/platform/App.java
+++ jlog-agent/src/main/java/com/logging/platform/App.java
@@ -1,7 +1,20 @@
 package com.logging.platform;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
+
+@Path("/hello")
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    private static final Logger LOG = Logger.getLogger(App.class);
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        LOG.info("Hello");
+        return "hello";
     }
 }
