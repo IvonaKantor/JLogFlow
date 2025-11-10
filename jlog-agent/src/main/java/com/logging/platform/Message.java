8d877d51ec407985507bfff017adcf479899221f
ivona <ik87056@student.uwb.edu.pl>
2025-11-10 02:45:49 +0100

    added a log message class

--- jlog-agent/src/main/java/com/logging/platform/Message.java
+++ jlog-agent/src/main/java/com/logging/platform/Message.java
@@ -4,5 +4,9 @@ import java.time.LocalDateTime;
 
 public class Message {
 
-
+    private String serviceName;
+    private String level;
+    private String message;
+    private LocalDateTime timestamp;
+    private String traceId;
 }
