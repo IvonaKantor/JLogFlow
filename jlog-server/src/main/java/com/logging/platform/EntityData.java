5ae38f2b11be8df6241f9c8af8612fb0b35ad55b
ivona <ik87056@student.uwb.edu.pl>
2025-11-12 21:46:27 +0100

    addedd an entity to the server

---
+++ jlog-server/src/main/java/com/logging/platform/EntityData.java
@@ -0,0 +1,14 @@
+package com.logging.platform;
+
+import jakarta.persistence.Entity;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import java.time.LocalDateTime;
+
+@Entity
+public class EntityData extends PanacheEntity {
+    public String serviceName;
+    public String level;
+    public String message;
+    public LocalDateTime timestamp;
+    public String traceId;
+}
