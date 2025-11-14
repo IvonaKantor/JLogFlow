406be9daf66447c5bd2ba3981277de6bd30d7c23
ivona <ik87056@student.uwb.edu.pl>
2025-11-14 23:58:03 +0100

    added a log writer service

--- jlog-agent/src/main/java/com/logging/platform/Writer.java
+++ jlog-agent/src/main/java/com/logging/platform/Writer.java
@@ -4,4 +4,11 @@ import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class Writer {
+
+    @Outgoing("out")
+    Emitter<Record<String, Message>> emitter;
+
+    public void writeLog(Message message){
+
+    }
 }
