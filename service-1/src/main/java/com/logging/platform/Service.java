e4bb0924dd8532822377ea6d0e6290cb4a14a1a9
ivona <ik87056@student.uwb.edu.pl>
2025-11-16 16:03:22 +0100

    added operation method to write new log

---
+++ service-1/src/main/java/com/logging/platform/Service.java
@@ -0,0 +1,28 @@
+package com.logging.platform;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/service1")
+@Produces(MediaType.APPLICATION_JSON)
+public class Service {
+
+    @Inject
+    Writer writer;
+
+    @GET
+    @Path("/operation")
+    public String operation() {
+        Message log = new Message();
+        log.setServiceName("service1");
+        log.setMessage("Hello World");
+        log.setLevel("INFO");
+
+        writer.writeLog(log);
+
+        return "Hello World";
+    }
+}
