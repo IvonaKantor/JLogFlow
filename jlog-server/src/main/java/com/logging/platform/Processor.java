5273d744745b3f51a690c7331632be48527ba483
ivona <ik87056@student.uwb.edu.pl>
2025-11-29 02:30:06 +0100

    sent returned objects to the logs channel

--- jlog-server/src/main/java/com/logging/platform/Processor.java
+++ jlog-server/src/main/java/com/logging/platform/Processor.java
@@ -2,12 +2,14 @@ package com.logging.platform;
 
 import jakarta.transaction.Transactional;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class Processor {
 
-    @Incoming("in")
+    @Incoming("requests")
+    @Outgoing("logs")
     @Transactional
     public void handle(Message message) {
         EntityData entity = new EntityData();
