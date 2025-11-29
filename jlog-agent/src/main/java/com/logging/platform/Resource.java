4f1ca9b967d81c79a45a844b1f6b4f024471362d
ivona <ik87056@student.uwb.edu.pl>
2025-11-29 02:05:42 +0100

    changed the message resource

--- jlog-agent/src/main/java/com/logging/platform/Resource.java
+++ jlog-agent/src/main/java/com/logging/platform/Resource.java
@@ -1,20 +1,32 @@
 package com.logging.platform;
 
-import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
-import org.jboss.logging.Logger;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import io.smallrye.reactive.messaging.kafka.Record;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import jakarta.ws.rs.*;
+import jakarta.inject.Inject;
 
 @Path("/api/logs")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class Resource {
 
-    private static final Logger LOG = Logger.getLogger(Resource.class);
+    @Inject
+    @Channel("message")
+    Emitter<Record<String, Message>> emitter;
+
+    @POST
+    @Path("/request")
+    public Response writeLog(Message message) {
+        emitter.send(Record.of(message.getServiceName(), message));
+        return Response.ok().entity("log sent").build();
+    }
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        LOG.info("Hello");
-        return "hello";
+    @Path("/health")
+    public Response health() {
+        return Response.ok().build();
     }
 }
