0916db530439a7fa9185e0cb7a8dccfb5c62cbb6
ivona <ik87056@student.uwb.edu.pl>
2025-12-14 01:10:43 +0100

    update

--- jlog-mdc-appender/src/main/java/com/logging/platform/filter/MetadataAppender.java
+++ jlog-mdc-appender/src/main/java/com/logging/platform/filter/MetadataAppender.java
@@ -1,4 +1,22 @@
 package com.logging.platform.filter;
 
-public class MetadataAppender {
+import io.quarkus.logging.LoggingFilter;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.util.logging.Filter;
+import java.util.logging.LogRecord;
+
+@LoggingFilter(name="filter")
+public class MetadataAppender implements Filter {
+
+    private final String part;
+
+    public MetadataAppender(@ConfigProperty(name = "filter.part") String part) {
+        this.part = part;
+    }
+
+    @Override
+    public boolean isLoggable(LogRecord record) {
+        return !record.getMessage().contains(part);
+    }
 }
