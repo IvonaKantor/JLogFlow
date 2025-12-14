155eb084896e821c44b4466372a4c5b65a7354b4
ivona <ik87056@student.uwb.edu.pl>
2025-12-14 16:02:58 +0100

    update

--- jlog-mdc-appender/src/main/java/com/logging/platform/filter/MetadataAppender.java
+++ jlog-mdc-appender/src/main/java/com/logging/platform/filter/MetadataAppender.java
@@ -1,13 +1,18 @@
 package com.logging.platform.filter;
 
-
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
 
-public class MetadataAppender extends Filter {
+public class MetadataAppender extends Filter<ILoggingEvent> {
 
     @Override
-    public FilterReply decide(Object o) {
-        return null;
+    public FilterReply decide(ILoggingEvent event) {
+        if (event.getLevel() == Level.ERROR) {
+            return FilterReply.ACCEPT;
+        } else {
+            return FilterReply.NEUTRAL;
+        }
     }
 }
