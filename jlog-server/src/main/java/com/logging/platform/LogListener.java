c6850cd2cf690e5c3c97bff4fb4f5c1c4212411a
ivona <ik87056@student.uwb.edu.pl>
2025-12-15 19:53:06 +0100

    implemented parse log data

--- jlog-server/src/main/java/com/logging/platform/LogListener.java
+++ jlog-server/src/main/java/com/logging/platform/LogListener.java
@@ -1,14 +1,22 @@
 package com.logging.platform;
 
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.logging.platform.models.LogData;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 @ApplicationScoped
 public class LogListener {
 
+    @Inject
+    ObjectMapper objectMapper;
+
     @Incoming("logs")
-    public void processLogEntry(String logEntry) {
-        System.out.println("New log: " + logEntry);
+    public void processLogEntry(String message) throws JsonProcessingException {
+        final var logEntry = objectMapper.readValue(message, LogData.class);
     }
+
+
 }
--- service-2/src/main/java/com/logging/platform/App.java
+++ service-2/src/main/java/com/logging/platform/App.java
@@ -2,8 +2,5 @@ package com.logging.platform;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+    private String level;
 }
