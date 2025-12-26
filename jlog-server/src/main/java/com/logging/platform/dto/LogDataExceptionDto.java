1e7217bca93c0d53911a10275831e9adbac5499d
ivona <ik87056@student.uwb.edu.pl>
2025-12-26 12:14:37 +0100

    added dto dataException class

--- jlog-server/src/main/java/com/logging/platform/dto/LogDataExceptionDto.java
+++ jlog-server/src/main/java/com/logging/platform/dto/LogDataExceptionDto.java
@@ -1,4 +1,42 @@
 package com.logging.platform.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.smallrye.common.constraint.NotNull;
+
 public class LogDataExceptionDto {
+
+    @NotNull
+    @JsonProperty("refId")
+    public Long refId;
+
+    @JsonProperty("exceptionType")
+    public String exceptionType;
+
+    @NotNull
+    @JsonProperty("message")
+    public String message;
+
+    public Long getRefId() {
+        return refId;
+    }
+
+    public void setRefId(Long refId) {
+        this.refId = refId;
+    }
+
+    public String getExceptionType() {
+        return exceptionType;
+    }
+
+    public void setExceptionType(String exceptionType) {
+        this.exceptionType = exceptionType;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
